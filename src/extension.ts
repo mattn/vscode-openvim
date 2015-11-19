d30d9e38bb62b323267d392a973136461cc4289b
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-11-19 18:26:46 +0900

    openvim.executablePath

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,14 @@ import cp = require('child_process');
 
 export function activate(ctx: vscode.ExtensionContext): void {
   ctx.subscriptions.push(vscode.commands.registerCommand("extension.openVim", () => {
+	var vim = vscode.workspace.getConfiguration('openvim')['executablePath'];
+    if (!vim) vim = 'gvim';
     var fileName = vscode.window.activeTextEditor.document.fileName;
-    if (fileName) cp.execFile("gvim", [fileName]);
+	if (fileName) {
+      cp.execFile(vim, [fileName], {}, (err, stdout, stderr) => {
+        if (err && (<any>err).code == "ENOENT")
+          vscode.window.showInformationMessage("'gvim' command is not available. you change modify openvim.execFile on user configuration.");
+      });
+    }
   }));
 }

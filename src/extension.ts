ba61a36204b432e78baa575c9261b43b53c7aa8d
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2015-11-26 09:36:07 +0900

    restore cursor location
    Close #1

--- src/extension.ts
+++ src/extension.ts
@@ -5,13 +5,16 @@ import cp = require('child_process');
 
 export function activate(ctx: vscode.ExtensionContext): void {
   ctx.subscriptions.push(vscode.commands.registerCommand("extension.openVim", () => {
-	var vim = vscode.workspace.getConfiguration('openvim')['executablePath'];
+    var vim = vscode.workspace.getConfiguration('openvim')['executablePath'];
     if (!vim) vim = 'gvim';
-    var fileName = vscode.window.activeTextEditor.document.fileName;
+    var editor = vscode.window.activeTextEditor;
+    var fileName = editor.document.fileName;
     if (fileName) {
-      cp.execFile(vim, [fileName], {}, (err, stdout, stderr) => {
+      var position = editor.selection.active;
+      var cursor = [0, position.line+1, position.character+1, 0]
+      cp.execFile(vim, ['-c', 'call setpos(".", ' + JSON.stringify(cursor) + ')', fileName], {}, (err, stdout, stderr) => {
         if (err && (<any>err).code == "ENOENT")
-          vscode.window.showErrorMessage("'gvim' command not available. you change modify openvim.execFile on user configuration.");
+          vscode.window.showErrorMessage("'gvim' command not available. you can modify openvim.execFile on user configuration.");
       });
     }
   }));
